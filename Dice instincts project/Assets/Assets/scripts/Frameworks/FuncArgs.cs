ea159af792684b72daa25420abc15c85ecd0e88f
C0nflex <108312736+C0nflex@users.noreply.github.com>
2023-08-01 01:24:44 +0300

    merge2

--- Dice instincts project/Assets/Assets/scripts/Frameworks/FuncArgs.cs
+++ Dice instincts project/Assets/Assets/scripts/Frameworks/FuncArgs.cs
@@ -7,10 +7,19 @@ public class FuncArgs : EventArgs
     public EventHandler<FuncArgs> FuncToRun;
     public int EffectNum;
     public EffectTiming Timing;
-    public FuncArgs(EventHandler<FuncArgs> funcToRun, int effectNum,EffectTiming timing)
+    public CharacterBehaviour character = null;
+    public Status status;
+    public FuncArgs(EventHandler<FuncArgs> funcToRun, int effectNum, EffectTiming timing)
     {
         FuncToRun = funcToRun;
         EffectNum = effectNum;
         Timing = timing;
+    }
+    public FuncArgs(EventHandler<FuncArgs> funcToRun, int effectNum, EffectTiming timing, Status status)
+    {
+        FuncToRun = funcToRun;
+        this.EffectNum = effectNum;
+        Timing = timing;
+        this.status = status;
     }
 }
